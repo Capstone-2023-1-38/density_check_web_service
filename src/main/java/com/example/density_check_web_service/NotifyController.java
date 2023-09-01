5e879254bdc62029989e9b7ce4aa62c95815df76
zigimi <chju0905@pusan.ac.kr>
2023-09-01 22:31:40 +0900

    fix: sse 사용 하면서 생기는 hikari pool 고갈 현상 수정

--- src/main/java/com/example/density_check_web_service/NotifyController.java
+++ src/main/java/com/example/density_check_web_service/NotifyController.java
@@ -2,12 +2,14 @@ package com.example.density_check_web_service;
 
 import com.example.density_check_web_service.domain.Notify.NotifyRepository;
 import com.example.density_check_web_service.domain.Notify.dto.NotifyDto;
+import com.example.density_check_web_service.domain.Users.UsersRepository;
 import com.example.density_check_web_service.service.NotifyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -21,8 +23,6 @@ import java.util.concurrent.ConcurrentHashMap;
 public class NotifyController {
     private final NotifyService notifyService;
 
-    public static Map<Long, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
-
     @ResponseBody
     @GetMapping(value = "/sub", produces = "text/event-stream")
     public SseEmitter subscribe(Authentication authentication,
@@ -30,17 +30,19 @@ public class NotifyController {
 
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String email = oAuth2User.getAttribute("email");
+//        long userId = notifyService.findId(email);
 
         SseEmitter sseEmitter = notifyService.subscribe(email);
 
         return sseEmitter;
     }
     @ResponseBody
-    @GetMapping("/notify")
-    public List<NotifyDto> notify(Authentication authentication)
+    @GetMapping("/notify/{id}")
+    public List<NotifyDto> notify(Authentication authentication, @PathVariable Long id)
     {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String email = oAuth2User.getAttribute("email");
-        return notifyService.notify(email);
+
+        return notifyService.notify(email, id);
     }
 }
--- src/main/java/com/example/density_check_web_service/service/NotifyService.java
+++ src/main/java/com/example/density_check_web_service/service/NotifyService.java
@@ -6,16 +6,22 @@ import com.example.density_check_web_service.domain.Notify.Notify;
 import com.example.density_check_web_service.domain.Notify.NotifyRepository;
 import com.example.density_check_web_service.domain.Notify.dto.NotifyDto;
 import com.example.density_check_web_service.domain.Posts.dto.PostsListResponseDto;
+import com.example.density_check_web_service.domain.Users.Users;
 import com.example.density_check_web_service.domain.Users.UsersRepository;
 import groovy.util.NodeList;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.User;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -23,8 +29,13 @@ import java.util.stream.Collectors;
 public class NotifyService {
     private final NotifyRepository notifyRepository;
     private final UsersRepository usersRepository;
-    @Transactional
-    public List<NotifyDto> notify(String email) {
+    public static Map<String, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
+
+    public List<NotifyDto> notify(String email, Long id) {
+        if (id != 0) {
+            notifyRepository.deleteByPostsId(id);
+            notifyRepository.flush();
+        }
         List<Notify> notify = notifyRepository.findAllByEmail(email);
         if (notify != null) {
             return notify.stream().map(NotifyDto::new).collect(Collectors.toList());
@@ -34,13 +45,10 @@ public class NotifyService {
         }
     }
 
-    @Transactional
     public SseEmitter subscribe(String email) {
 
-        long userId = usersRepository.findByEmail(email).get().getId();
-
-        if (NotifyController.sseEmitters.containsKey(userId)) {
-            return NotifyController.sseEmitters.get(userId);
+        if (sseEmitters.containsKey(email)) {
+            return sseEmitters.get(email);
         }
 
         // 현재 클라이언트를 위한 SseEmitter 생성
@@ -53,12 +61,24 @@ public class NotifyService {
         }
 
         // user의 pk값을 key값으로 해서 SseEmitter를 저장
-        NotifyController.sseEmitters.put(userId, sseEmitter);
+        sseEmitters.put(email, sseEmitter);
 
-        sseEmitter.onCompletion(() -> NotifyController.sseEmitters.remove(userId));
-        sseEmitter.onTimeout(() -> NotifyController.sseEmitters.remove(userId));
-        sseEmitter.onError((e) -> NotifyController.sseEmitters.remove(userId));
+        sseEmitter.onCompletion(() -> sseEmitters.remove(email));
+        sseEmitter.onTimeout(() -> sseEmitters.remove(email));
+        sseEmitter.onError((e) -> sseEmitters.remove(email));
 
         return sseEmitter;
     }
+
+//    @Async
+//    @EventListener
+//    public void catchEvent(Notify notify){
+//        Users user = notify.getUsers();
+//        SseEmitter sseEmitter = NotifyService.sseEmitters.get(user.getId());
+//        try {
+//            sseEmitter.send(SseEmitter.event().name("notification").data(new NotifyDto(notify)));
+//        } catch (Exception e) {
+//            NotifyService.sseEmitters.remove(user.getId());
+//        }
+//    }
 }
