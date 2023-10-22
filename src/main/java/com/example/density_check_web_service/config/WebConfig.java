cd1c0c81bfed6225f206308ee86fe0b6b0d41901
zigimi <chju0905@pusan.ac.kr>
2023-10-23 00:19:36 +0900

    fix: 프로필 이미지 저장 주소 수정

--- src/main/java/com/example/density_check_web_service/config/WebConfig.java
+++ src/main/java/com/example/density_check_web_service/config/WebConfig.java
@@ -6,11 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private final String fileDir = System.getProperty("user.dir") + "/src/main/resources/static/img/profile/";
+    private final String fileDir = System.getProperty("user.dir") + "/density_check_web_service/src/main/resources/static/img/profile/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        System.out.println(fileDir);
         registry.addResourceHandler("/img/profile/**").addResourceLocations("file:/"+fileDir);
     }
 }
