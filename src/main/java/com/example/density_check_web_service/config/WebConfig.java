59c2576e29e830cce2f7891ccfa4c99eb62c15bd
zigimi <chju0905@pusan.ac.kr>
2023-10-23 01:51:41 +0900

    fix: 이미지 저장 주소 수정

--- src/main/java/com/example/density_check_web_service/config/WebConfig.java
+++ src/main/java/com/example/density_check_web_service/config/WebConfig.java
@@ -14,6 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
 //    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("img/profile/**").addResourceLocations(fileDir);
+        registry.addResourceHandler("/img/profile/**").addResourceLocations("file:"+fileDir);
     }
 }
